268e220889f854235049994660a43146d86ffaee
YellowDi <yellowdi@me.com>
2017-06-17 07:04:08 +0800

    KuiNameplates : update version

--- Interface/AddOns/Kui_Nameplates_Core_Config/create.lua
+++ Interface/AddOns/Kui_Nameplates_Core_Config/create.lua
@@ -7,7 +7,7 @@ version:SetAlpha(.5)
 version:SetPoint('TOPRIGHT',-12,-10)
 version:SetText(string.format(
     opt.titles.version,
-    'KuiNameplates','Kesava','2.14.11'
+    'KuiNameplates','Kesava','2.14.11.1'
 ))
 
 opt:Initialise()
