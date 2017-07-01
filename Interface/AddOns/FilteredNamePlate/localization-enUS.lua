55c9fcc994407a8b4bd47505264217db447c8054
YellowDi <yellowdi@me.com>
2017-07-01 17:07:34 +0800

    FilteredNamePlate : new add

---
+++ Interface/AddOns/FilteredNamePlate/localization-enUS.lua
@@ -0,0 +1,42 @@
+﻿-- Prevent tainting global _.
+if GetLocale() == "enUS" then
+FNP_LOCALE_TEXT.FNP_ENABLE_TXT = "Enable";
+
+FNP_LOCALE_TEXT.FNP_TOOLTIP_TIDY = "Using with TidyPlate, check this.";
+FNP_LOCALE_TEXT.FNP_TOOLTIP_KUI = "Using with Kui_namePlates, check this";
+FNP_LOCALE_TEXT.FNP_ORIG_TITLE = "Original";
+FNP_LOCALE_TEXT.FNP_TOOLTIP_ORIG = "Using with original UI, check this";
+FNP_LOCALE_TEXT.FNP_TOOLTIP_EUI_RAYUI = "Using with ElvUI or RayUI, check this";
+FNP_LOCALE_TEXT.FNP_TOOLTIP_NDUI = "Using with NDUI, check this";
+
+FNP_LOCALE_TEXT.FNP_DEFAULT_SCALE_TXT = "Default Scale";
+FNP_LOCALE_TEXT.FNP_DEFAULT_SCALE_TOOLTIP = "Scale the normal unit Unit";
+FNP_LOCALE_TEXT.FNP_ONLYSHOW_OTHER_SCALE_TXT = "Non-OnlyShow Scale";
+FNP_LOCALE_TEXT.FNP_ONLYSHOW_OTHER_SCALE_TOOLTIP = "Scale the non-OnlyShow unit when only show works";
+FNP_LOCALE_TEXT.FNP_ONLYSHOW_SCALE_TXT = "Scale the OnlyShow unit";
+FNP_LOCALE_TEXT.FNP_ONLYSHOW_SCALE_TOOLTIP = "Scale the OnlyShow unit when only show works";
+
+FNP_LOCALE_TEXT.FNP_TAKEEFFECT_BTN = "Active";
+FNP_LOCALE_TEXT.FNP_RELOAD_BTN = "ReloadUI";
+
+FNP_LOCALE_TEXT.FNP_TXT_BIG = "Big";
+FNP_LOCALE_TEXT.FNP_TXT_SMALL = "Small";
+
+FNP_LOCALE_TEXT.FNP_ONLYSHOW_LIST_TXT = "OnlyShow List";
+FNP_LOCALE_TEXT.FNP_FILTER_LIST_TXT = "Filter List";
+FNP_LOCALE_TEXT.FNP_STRING_UI_TYPE = "UI Type(reload after changed)";
+FNP_LOCALE_TEXT.FNP_STRING_NOTE = "using ; to add a name";
+
+FNP_LOCALE_TEXT.FNP_STRING_AUTHOR_VER = "Author:Allan version:7.2.5.20170630 V5";
+
+FNP_LOCALE_TEXT.FNP_PRINT_ERROR_UITYPE = "\124cFFF58CBA[ /fnp ]Error！Your UI type does not match, please check the UI type correctly and reloadUI!！\124r";
+FNP_LOCALE_TEXT.FNP_PRINT_UITYPE_CHANGED = "\124cFFF58CBAUI type is changed，please reloadUI !\124r";
+FNP_LOCALE_TEXT.FNP_PRINT_HELP0 = "\124cFFF58CBA[FilteredNamePlates]\124r";
+FNP_LOCALE_TEXT.FNP_PRINT_HELP1 = "\124cFFF58CBA/fnp options or /fnp opt \124r open the setting menu";
+FNP_LOCALE_TEXT.FNP_PRINT_HELP2 = "\124cFFF58CBA/fnp change or /fnp ch \124r quickly to enable or disable";
+FNP_LOCALE_TEXT.FNP_PRINT_HELP3 = "\124cFFF58CBA/fnp refresh \124rquickly to hide the casting units";
+
+FNP_LOCALE_TEXT.FNP_TEXT_HELP = "Help>";
+
+FNP_LOCALE_TEXT.FNP_TEXT_INFO0 = "If the setting does not take effect, you can switch the shortcut key to display and hide healthbar(nameplates). If you still have some flaws, try Reload";
+end
