c8585d405bd5b7ff7f6c93dcd22f16d5896dd278
YellowDi <yellowdi@me.com>
2018-10-09 00:30:41 +0800

    更新 Sora's

---
+++ Interface/AddOns/Sora's/Init.lua
@@ -0,0 +1,6 @@
+﻿-- Engines
+local _, ns = ...
+ns[1] = {} -- S, Misc
+ns[2] = {} -- C, Config
+ns[3] = {} -- L, Locale
+ns[4] = {} -- DB, DataBase
---
+++ Interface/AddOns/Sora's/Lib/DB.lua
@@ -0,0 +1,7 @@
+﻿-- Engine
+local S, C, L, DB = unpack(select(2, ...))
+
+-- Begin
+DB.GlowTex = "InterFace\\AddOns\\Sora's\\Media\\GlowTex"
+DB.Statusbar = "InterFace\\AddOns\\Sora's\\Media\\Statusbar"
+DB.AuraFont = "Interface\\Addons\\Sora's\\Media\\ROADWAY.TTF"
